925b5111d7130f2b577c9fcf10140f3c0c3bdce7
sumitydv14 <sumityadavmmh265@gmail.com>
2026-06-15 19:41:46 +0530

    fix swagger production path

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,32 +1,33 @@
-import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerJsdoc from "swagger-jsdoc";
 
 const options: swaggerJsdoc.Options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Auth Service API",
-            version: "1.0.0",
-            description: "Node.js + PostgreSQL Authentication API"
-        },
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: "http",
-                    scheme: "bearer",
-                    bearerFormat: "JWT",
-                },
-            },
-        },
-
-        servers: [
-            {
-                url: "http://localhost:5000",
-            },
-        ],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Auth Service API",
+      version: "1.0.0",
+      description: "Node.js + PostgreSQL Authentication API",
     },
-    apis: [
-        "./src/modules/**/*.ts",
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    servers: [
+      {
+        url: process.env.API_URL || "http://localhost:5000",
+      },
     ],
+  },
+
+  apis:
+    process.env.NODE_ENV === "production"
+      ? ["./dist/modules/**/*.js"]
+      : ["./src/modules/**/*.ts"],
 };
 
-export const swaggerSpec = swaggerJsdoc(options)
+export const swaggerSpec = swaggerJsdoc(options);
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from './app'
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,  () => {
     console.log(`Server is running on port ${PORT}`);
 }) 
 
